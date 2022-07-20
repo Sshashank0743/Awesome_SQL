bf46cfc7607539509b7ed920e94351b254e2b03d
shashank shukla <53620609+Sshashank0743@users.noreply.github.com>
2022-07-20 22:47:47 +0530

    Add files via upload

---
+++ Table Create/SELECT.sql
@@ -0,0 +1,9 @@
+SELECt * FROM customers;
+
+SELECT CustomerName, City FROM Customers;
+
+SELECT Country FROM Customers;
+
+SELECT DISTINCT Country FROM Customers;
+
+SELECT Count(DISTINCT Country) FROM Customers;
